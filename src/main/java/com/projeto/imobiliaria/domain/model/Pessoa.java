7debb8c0e80b656c0573b1fb55044b1c1f25246d
joao-pcd <jclementinodias@gmail.com>
2024-06-24 18:13:33 -0300

    Implementação dos Getters e Setter da classe Pessoa

--- src/main/java/com/projeto/imobiliaria/domain/model/Pessoa.java
+++ src/main/java/com/projeto/imobiliaria/domain/model/Pessoa.java
@@ -1,8 +1,12 @@
 package com.projeto.imobiliaria.domain.model;
 
 import jakarta.persistence.Column;
+import lombok.Getter;
+import lombok.Setter;
 
 
+@Getter
+@Setter
 public abstract class Pessoa {
 
     @Column(nullable = false, length = 50)
