db3641175e480ca84e0551aade21a8fef900f44d
joao-pcd <jclementinodias@gmail.com>
2024-06-24 18:12:52 -0300

    Atualizando variáveis Endereco

--- src/main/java/com/projeto/imobiliaria/domain/model/Endereco.java
+++ src/main/java/com/projeto/imobiliaria/domain/model/Endereco.java
@@ -15,7 +15,12 @@ public class Endereco {
     @Id
     private String cep;
     private String logradouro;
-    private String numero;
-    private String cidade;
-    private String estado;
+    private String complemento;
+    private String bairro;
+    private String localidade;
+    private String uf;
+    private String ibge;
+    private String gia;
+    private String ddd;
+    private String siafi;
 }
