64f76c6c21a0c2a0c4ec854415e69e3de7f8d8fa
joao-pcd <jclementinodias@gmail.com>
2024-06-26 20:11:06 -0300

    Criação do método de busca por imóvel

--- src/main/java/com/projeto/imobiliaria/domain/repository/ReservaRepository.java
+++ src/main/java/com/projeto/imobiliaria/domain/repository/ReservaRepository.java
@@ -1,7 +1,9 @@
 package com.projeto.imobiliaria.domain.repository;
 
+import com.projeto.imobiliaria.domain.model.Imovel;
 import com.projeto.imobiliaria.domain.model.Reserva;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReservaRepository extends JpaRepository<Reserva, Long> {
+    Reserva findByImovelId(Imovel imovel);
 }
